a284c1efe35044a85572f0f82693e2d2fa7ca0cd
Matthew J. Berger <matthewberger@nevada.unr.edu>
2015-08-29 10:09:34 -0700

    Add Shader Program wrapper class.

--- src/Shader.h
+++ src/Shader.h
@@ -2,6 +2,11 @@
 #define SHADER_H
 
 #include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp> // Makes passing matrices to shaders easier
+
 #include <string>
 #include <fstream>
 
---
+++ src/ShaderProgram.h
@@ -0,0 +1,57 @@
+#ifndef __SHADER_PROGRAM_H
+#define __SHADER_PROGRAM_H
+
+#include "Shader.h"
+
+class ShaderProgram
+{
+    public:
+        ShaderProgram();
+        ~ShaderProgram();
+
+        void CreateProgram();
+        void DeleteProgram();
+
+        bool AddShader(Shader* shader);
+        bool LinkProgram();
+
+        void PrintLog();
+
+        void UseProgram();
+        void DisUseProgram();
+
+        // Set floats
+        void SetUniform(std::string name, float* values, int count=1);
+        void SetUniform(std::string name, const float value);
+
+        // Set vectors
+        void SetUniform(std::string name, glm::vec2* vectors, int count=1);
+        void SetUniform(std::string name, const glm::vec2 vector);
+
+        void SetUniform(std::string name, glm::vec3* vectors, int count=1);
+        void SetUniform(std::string name, const glm::vec3 vector);
+
+        void SetUniform(std::string name, glm::vec4* vectors, int count=1);
+        void SetUniform(std::string name, const glm::vec4 vector);
+
+        // Set 4x4 matrices
+        void SetUniform(std::string name, glm::mat4* matrices, int count=1);
+        void SetUniform(std::string name, const glm::mat4 matrix);
+
+        // Set integers
+        void SetUniform(std::string name, int* values, int count=1);
+        void SetUniform(std::string name, const int value);
+
+        GLuint GetID()
+        {
+            return programID;
+        }
+
+    private:
+
+        GLuint programID;
+
+        bool linked;
+};
+
+#endif
