0f7daef641a6a1afd1411013fe5295631ed43ef6
Matthew J. Berger <pacoishere@gmail.com>
2015-08-29 21:00:07 -0700

    Define GLM_FORCE_RADIANS for compatibility with the latest version of glm.

--- src/core/GlobalIncludes.h
+++ src/core/GlobalIncludes.h
@@ -1,6 +1,7 @@
 #include <GL/glew.h> // Glew must be included before the main GL libs
 #include <GL/glut.h> // Doing otherwise causes compiler errors
 
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp> // Makes passing matrices to shaders easier
